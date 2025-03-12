d6e877b3bd054a068e55aa5486e4c03679fd6840
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-12 23:37:47 +0000

    change nom de class

--- src/main/java/net/me/gestionconsultationbdcc/Application.java
+++ src/main/java/net/me/gestionconsultationbdcc/Application.java
@@ -1,6 +1,5 @@
 package net.me.gestionconsultationbdcc;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,13 +8,13 @@ import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 440);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-        stage.setTitle("main view!");
+        stage.setTitle("gestion consultation");
         stage.setScene(scene);
         stage.show();
     }
